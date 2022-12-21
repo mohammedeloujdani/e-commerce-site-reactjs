1e1bbc53ec3a75b49b4a1abcfef3dac91e80fd02
mohammedeloujdani <mohammed.eloujdani1@gmail.com>
2022-12-21 13:07:35 +0100

    Updates

---
+++ precache-manifest.ca9fbf126af40418352c5d20136f2a70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9635e7d2a586ac50184c",
+    "url": "/e-commerce-site-reactjs/static/css/main.f69dde3a.chunk.css"
+  },
+  {
+    "revision": "9635e7d2a586ac50184c",
+    "url": "/e-commerce-site-reactjs/static/js/main.9635e7d2.chunk.js"
+  },
+  {
+    "revision": "fad9f47f95d12fe10b61",
+    "url": "/e-commerce-site-reactjs/static/css/1.964cd3ee.chunk.css"
+  },
+  {
+    "revision": "fad9f47f95d12fe10b61",
+    "url": "/e-commerce-site-reactjs/static/js/1.fad9f47f.chunk.js"
+  },
+  {
+    "revision": "25d174c3a2b081b14bdb",
+    "url": "/e-commerce-site-reactjs/static/js/runtime~main.25d174c3.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/e-commerce-site-reactjs/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "1e0b4771003467a1cd9a2a7b0b4f4f9a",
+    "url": "/e-commerce-site-reactjs/index.html"
+  }
+];
